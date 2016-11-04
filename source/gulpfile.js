b629ef2169fac50086ee211ada56011c53729794
江文得 <82697451@qq.com>
2016-11-04 16:24:02 +0800

    第二版gulpfile.js

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
 var browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', ['sass'], function () {
@@ -24,14 +25,16 @@ gulp.task('sass', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('copy', function () {
-    gulp.src('*.html')
+// 把/source/pages/下的文件套上公共部分放到根目录下
+gulp.task('build', function () {
+    gulp.src('pages/*.html')
+        .pipe(wrap({src:'layout/layout.html'}))
         .pipe(gulp.dest('../'));
 });
 
+// 监控/source/任意文件夹/下的html文件，若有变化则执行build和自动刷新
 gulp.task('watch', function () {
-    gulp.watch(['*.html'], ['copy', browserSync.reload]);
-    gulp.watch(['style/*.scss'], ['sass']);
+    gulp.watch(['**/*.html'], ['build', browserSync.reload]);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
